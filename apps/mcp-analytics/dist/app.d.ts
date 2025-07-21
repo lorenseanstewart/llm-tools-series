12ecf670d4b8ed88c0192b714c040570d5114535
lstew111 <loren.stewart@engelvoelkers.com>
2025-07-20 18:58:59 -0700

    removed node_modules and dist folders

--- apps/mcp-analytics/dist/app.d.ts
+++
@@ -1,2 +0,0 @@
-import { FastifyInstance } from 'fastify';
-export declare function build(opts?: {}): FastifyInstance<import("fastify").RawServerDefault, import("http").IncomingMessage, import("http").ServerResponse<import("http").IncomingMessage>, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault>;
--- apps/mcp-listings/dist/tools/listings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendListingReport = exports.findListings = void 0;
-// Re-export all listings tools for easier importing
-var find_listings_1 = require("./find-listings");
-Object.defineProperty(exports, "findListings", { enumerable: true, get: function () { return find_listings_1.findListings; } });
-var send_listing_report_1 = require("./send-listing-report");
-Object.defineProperty(exports, "sendListingReport", { enumerable: true, get: function () { return send_listing_report_1.sendListingReport; } });
